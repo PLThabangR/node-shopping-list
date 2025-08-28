4c37094870c61570700f7d9bc1f0fb1cf5cb1704
PLThabangR <rakgoropothabang@gmail.com>
2025-08-29 01:13:44 +0200

    creating my server in the index

--- src/server.ts
+++ src/server.ts
@@ -4,3 +4,11 @@ import http,{IncomingMessage,ServerResponse} from 'http';
 
 
 
+const server = http.createServer((req:IncomingMessage,res:ServerResponse)=>{
+    res.writeHead(200,{'Content-Type':'json/application'});
+    res.end('Hello World');
+});
+
+server.listen(8000);
+
+
