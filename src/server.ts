91b0abe4a741a32058318e870c3a9c71bc5ca342
PLThabangR <rakgoropothabang@gmail.com>
2025-08-29 02:31:18 +0200

    added logic in our server to send incoming request to the routes logic

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import http,{IncomingMessage,ServerResponse} from 'http';
+import { productRoutes } from './routes/productRoutes';
 
 
 const PORT=6000
 
 //creating a server
 const server = http.createServer((req:IncomingMessage,res:ServerResponse)=>{
-    res.writeHead(200,{'Content-Type':'json/application'});
-    res.end(JSON.stringify({message:"Hello World"}));
+if(req.url?.startsWith("/products")){
+    //call product routes logic
+    productRoutes(req,res)
+}else{
+    //if route not found 
+    res.writeHead(404,{'Content-Type':'application/json'})
+    res.end(JSON.stringify({message:"Route not found"}))
+}
 });
 
 
